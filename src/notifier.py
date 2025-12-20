87d35abe920661f203cba90dd2e4a29904e855cd
Szymon <czarnydm@gmail.com>
2025-12-20 21:36:55 +0000

    .

--- src/notifier.py
+++ src/notifier.py
@@ -5,32 +5,35 @@ TOKEN = "acdq1x3cy1u4gkvckqxoe89j1g6v7k"
 USER_KEY = "u2i3tny2nc7deaktnr41ur5ier3d1k"
 
 def send_notification(alert, islogo, airline):
+    print('*' * 50)
     print(islogo[1])
     print(type(islogo[1]))
-    # if islogo[1] is True:
-    #     logging.info(f"Sending notification with an image for {airline}")
-    #     print(f"Sending notification with an image for {airline}")
+    print('*' * 50)
 
-    #     r = requests.post("https://api.pushover.net/1/messages.json", data = {
-    #     "token": TOKEN,
-    #     "user": USER_KEY,
-    #     "message": alert,
-    #     },
+    if islogo[1] is True:
+        logging.info(f"Sending notification with an image for {airline}")
+        print(f"Sending notification with an image for {airline}")
 
-    #     files = {
-    #     "attachment": (f"./airline_logo.jpg", open(f"./airline_logo.jpg", "rb"), "image/jpeg")
-    #     }
-    # )
+        r = requests.post("https://api.pushover.net/1/messages.json", data = {
+        "token": TOKEN,
+        "user": USER_KEY,
+        "message": alert,
+        },
 
-    # else:
-    #     logging.info(f"Private airline or no logo found. Sending notification without an image for {airline}")
-    #     print(f"Private airline or no logo found. Sending notification without an image for {airline}")
+        files = {
+        "attachment": (f"./airline_logo.jpg", open(f"./airline_logo.jpg", "rb"), "image/jpeg")
+        }
+    )
 
-    #     r = requests.post("https://api.pushover.net/1/messages.json", data = {
-    #     "token": TOKEN,
-    #     "user": USER_KEY,
-    #     "message": alert,
-    #     }
-    # )
+    else:
+        logging.info(f"Private airline or no logo found. Sending notification without an image for {airline}")
+        print(f"Private airline or no logo found. Sending notification without an image for {airline}")
+
+        r = requests.post("https://api.pushover.net/1/messages.json", data = {
+        "token": TOKEN,
+        "user": USER_KEY,
+        "message": alert,
+        }
+    )
 
     
