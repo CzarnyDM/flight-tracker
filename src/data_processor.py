885b7c8fff98a1856745c0e22d690678901d0c3b
Szymon <czarnydm@gmail.com>
2025-12-20 20:26:38 +0000

    .

--- src/data_processor.py
+++ src/data_processor.py
@@ -90,8 +90,8 @@ def message(flight_data):
     
 
     logging.info(f"Airline: {flight_data['airline_name']}, Callsign: {flight_data['callsign']}, Flight number: {flight_data['number']}, Aircraft Type: {flight_data['aircraft']}, From: {flight_data['origin']}, To: {flight_data['destination']} ,Flight status: {flight_data['flight_status']}, Flight level: {flight_data['flight_level']}")
-    print(msg, logo)
-    return msg, logo
+    print(msg)
+    return msg
 
 def get_logo_image(airline_iata, airline_icao):
     try:
